a4ad376a4995d70c66cc602a067286fa692d8b6f
MeghdadSaniiUSD <msanii@sandiego.edu>
2024-02-04 15:09:07 -0800

    Add project files.

---
+++ Controllers/HomeController.cs
@@ -0,0 +1,66 @@
+﻿using MyStandardFramework_POC.Data_Layer;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+using System.Web.Mvc;
+
+namespace MyStandardFramework_POC.Controllers
+{
+	public class HomeController : Controller
+	{
+		public ActionResult Index()
+		{
+			List<GetMostPopularJob_Result> popularJob = new List<GetMostPopularJob_Result>();
+
+			using (var db = new MyDatabaseEntities())
+			{
+				var query = db.GetMostPopularJob();
+
+				foreach (GetMostPopularJob_Result item in query)
+				{
+					popularJob.Add(item);
+				}
+			}
+
+			return View(popularJob);
+		}
+
+		public ActionResult Users()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			List<Person> users = new List<Person>();
+
+			using (var db = new MyDatabaseEntities())
+			{
+				var query = from b in db.People
+							orderby b.first_name
+							select b;
+
+				foreach (Person item in query)
+				{
+					users.Add(item);
+				}
+			}
+
+			return View(users);
+		}
+
+		public ActionResult About()
+		{
+			ViewBag.Message = "Your application description page.";
+
+			return View();
+		}
+
+		public ActionResult Contact()
+		{
+			ViewBag.Message = "Your contact page.";
+
+			return View();
+		}
+
+
+	}
+}
---
+++ Global.asax
@@ -0,0 +1,1 @@
+﻿<%@ Application Codebehind="Global.asax.cs" Inherits="MyStandardFramework_POC.MvcApplication" Language="C#" %>
